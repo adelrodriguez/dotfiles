fcd84acb8ff3864d50418acdc8d61a1f9c84b881
Adel Rodríguez <hello@adelrodriguez.com>
2026-01-26 18:57:12 -0400

    chore: add fish config

---
+++ config/fish/conf.d/10-paths.fish
@@ -0,0 +1,7 @@
+if test -d "$HOME/.bun/bin"
+  fish_add_path "$HOME/.bun/bin"
+end
+
+if test -d "$HOME/.local/bin"
+  fish_add_path "$HOME/.local/bin"
+end
---
+++ config/fish/conf.d/90-atuin.fish
@@ -0,0 +1,4 @@
+if type -q atuin
+  set -gx ATUIN_SYNC_ENABLE false
+  atuin init fish | source
+end
