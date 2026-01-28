215eae07eeda4a4042c52cf2ff96bc3b2827962a
Adel Rodríguez <hello@adelrodriguez.com>
2026-01-28 16:24:02 -0400

    feat: add atuin to PATH and Ubuntu tool aliases
    - Add ~/.atuin/bin to fish PATH for atuin support
    - Add aliases for Ubuntu's batcat and fdfind commands

--- home/.config/fish/conf.d/10-paths.fish
+++ home/.config/fish/conf.d/10-paths.fish
@@ -9,3 +9,7 @@ end
 if test -d /Applications/Obsidian.app/Contents/MacOS
     fish_add_path /Applications/Obsidian.app/Contents/MacOS
 end
+
+if test -d "$HOME/.atuin/bin"
+    fish_add_path "$HOME/.atuin/bin"
+end
