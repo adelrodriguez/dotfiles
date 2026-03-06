2cd5dc77fe87270b25e534ea7d08758e1c979175
Adel Rodríguez <hello@adelrodriguez.com>
2026-03-06 18:54:49 -0400

    fix: Fish Homebrew shellenv syntax

--- home/.config/fish/conf.d/05-homebrew.fish
+++ home/.config/fish/conf.d/05-homebrew.fish
@@ -1,6 +1,6 @@
 for brew_bin in /opt/homebrew/bin/brew /usr/local/bin/brew /home/linuxbrew/.linuxbrew/bin/brew $HOME/.linuxbrew/bin/brew
     if test -x $brew_bin
-        eval "($brew_bin shellenv fish)"
+        eval ($brew_bin shellenv fish)
         break
     end
 end
