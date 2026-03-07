ff9b0fbbd231f09d66a80d0d42a0cb316ac11945
Adel Rodríguez <hello@adelrodriguez.com>
2026-03-07 11:59:38 -0400

    fix: restore fish completions in new shells
    Keep fish_complete_path unexported and normalize inherited values so custom completions autoload in fresh sessions.

--- home/.config/fish/conf.d/80-completions.fish
+++ home/.config/fish/conf.d/80-completions.fish
@@ -1,3 +1,21 @@
+set -l normalized_completion_paths
+
+for completion_path in $fish_complete_path
+    if test -d "$completion_path"
+        set -a normalized_completion_paths $completion_path
+    else if string match -q "* *" -- $completion_path
+        for split_path in (string split ' ' -- $completion_path)
+            if test -d "$split_path"
+                set -a normalized_completion_paths $split_path
+            end
+        end
+    end
+end
+
+if test (count $normalized_completion_paths) -gt 0
+    set -g --unexport fish_complete_path $normalized_completion_paths
+end
+
 for brew_completions in \
     /opt/homebrew/share/fish/vendor_completions.d \
     /usr/local/share/fish/vendor_completions.d \
@@ -5,7 +23,7 @@ for brew_completions in \
     $HOME/.linuxbrew/share/fish/vendor_completions.d
     if test -d $brew_completions
         if not contains -- $brew_completions $fish_complete_path
-            set -gx fish_complete_path $fish_complete_path $brew_completions
+            set -g --unexport fish_complete_path $fish_complete_path $brew_completions
         end
     end
 end
--- home/.config/fish/conf.d/85-orbstack.fish
+++ home/.config/fish/conf.d/85-orbstack.fish
@@ -2,6 +2,6 @@ set -l orbstack_completions /Applications/OrbStack.app/Contents/Resources/comple
 
 if test -d $orbstack_completions
     if not contains -- $orbstack_completions $fish_complete_path
-        set -gx fish_complete_path $fish_complete_path $orbstack_completions
+        set -g --unexport fish_complete_path $fish_complete_path $orbstack_completions
     end
 end
