8ab23bfb8e890a790b34738e0a2f9af06da70dcc
Adel Rodríguez <hello@adelrodriguez.com>
2026-03-12 17:08:29 -0400

    feat: auto-attach tmux on SSH sessions

--- home/.config/fish/conf.d/95-tmux-autostart.fish
+++ home/.config/fish/conf.d/95-tmux-autostart.fish
@@ -1,4 +1,4 @@
-if status is-interactive; and not set -q TMUX; and not set -q SSH_TTY; and command -q tmux
+if status is-interactive; and not set -q TMUX; and command -q tmux
     if tmux ls >/dev/null 2>&1
         tmux attach
     else
