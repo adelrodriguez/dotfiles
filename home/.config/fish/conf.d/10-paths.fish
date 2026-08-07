bcc9b28500727daa60db6f6101c52863cd11eae4
Adel Rodríguez <hello@adelrodriguez.com>
2026-08-06 23:05:50 -0400

    add cargo

--- home/.config/fish/conf.d/10-paths.fish
+++ home/.config/fish/conf.d/10-paths.fish
@@ -2,6 +2,10 @@ if test -d "$HOME/.bun/bin"
     fish_add_path "$HOME/.bun/bin"
 end
 
+if test -d "$HOME/.cargo/bin"
+    fish_add_path "$HOME/.cargo/bin"
+end
+
 if test -d "$HOME/.local/bin"
     fish_add_path "$HOME/.local/bin"
 end
