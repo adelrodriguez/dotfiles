76a3d9a9c889c965aa8ef3509288bd2dd6382a7d
Adel Rodríguez <hello@adelrodriguez.com>
2026-08-20 09:54:28 -0400

    fix: pnpm path

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,13 @@
 set -gx CODEX_HOME ~/.config/codex
+set -gx pnpm_config_pm_on_fail download
 
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+# pnpm
+set -gx PNPM_HOME '/Users/adelrodriguez/Library/pnpm'
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
